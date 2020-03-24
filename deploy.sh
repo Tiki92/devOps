e765099ca6b84fcced45848e1d8823255b98421c
Burca Vlad <tikiguy1642@gmail.com>
2020-03-24 18:37:03 +0200

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook playbook.yml --private-key /home/ec2-user/AWS_Ansible/aws_devOps.pem -u ec2-user  --vault-password-file /home/ec2-user/AWS_Ansible/vault.pass
+ansible-playbook playbook.yml --private-key /home/ec2-user/AWS_Ansible/aws_devOps.pem -u ec2-user  --vault-password-file /var/lib/jenkins/workspace/vault.pass
 
