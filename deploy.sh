bb727247f93e14debfe98c1e3aaebd84a3195ea3
Burca Vlad <tikiguy1642@gmail.com>
2020-03-25 10:37:04 +0200

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-ansible-playbook /var/lib/jenkins/workspace/dockerCompose/playbook.yml -i /var/lib/jenkins/workspace/dockerCompose/inventory --private-key /var/lib/jenkins/workspace/devOps.pem -u ec2-user || exit 0
+try {
+ansible-playbook /var/lib/jenkins/workspace/dockerCompose/playbook.yml -i /var/lib/jenkins/workspace/dockerCompose/inventory --private-key /var/lib/jenkins/workspace/devOps.pem -u ec2-user
+} catch(e) {
+build_ok = false
+echo e.toString()
+}
 echo "Deploy Success" || exit 0
