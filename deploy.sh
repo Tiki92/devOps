036b1825d3914135a1017cb2fd228a0407a461b2
Burca Vlad <tikiguy1642@gmail.com>
2020-03-25 11:37:23 +0200

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 deploy() {
-ansible-playbook /var/lib/jenkins/workspace/dockerCompose/playbook.yml -i /var/lib/jenkins/workspace/dockerCompose/inventory --private-key /var/lib/jenkins/workspace/devOps.pem -u ec2-user &
-BACK_PID=$!
-while kill -0 $BACK_PID ; do
-    echo "Process is still active..."
-    sleep 1
-done
-echo "Deploy Success"
-return 0
+ansible-playbook /var/lib/jenkins/workspace/dockerCompose/playbook.yml -i /var/lib/jenkins/workspace/dockerCompose/inventory --private-key /var/lib/jenkins/workspace/devOps.pem -u ec2-user || :
+
 }
 
 deploy
